a5a94f404d34d29622af0babacee354ae4d158e0
ITRepablik <itrepablik@gmail.com>
2020-02-27 14:17:14 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/itrepablik/itrlog
+
+go 1.13
+
+require (
+	github.com/itrepablik/lumberjack v0.0.0-20200227054043-61b5657340f7
+	go.uber.org/zap v1.14.0
+)
---
+++ logger.go
@@ -0,0 +1,34 @@
+// Package itrlog is the custom logger for Go using Zap and Lumberjack libraries.
+package itrlog
+
+import (
+	"time"
+
+	"github.com/itrepablik/lumberjack"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// LogTimeFormat formats the event timestamp.
+const LogTimeFormat string = "Jan-02-2006 03:04:05 PM"
+
+// InitLog initialize the zap and lumberjack logger library.
+// Example log file: logs/app_name_2020-02-28.log
+func InitLog(maxSize, maxAge int, logFolderName, logInitial string) *zap.Logger {
+	logFile := logFolderName + "/" + logInitial + time.Now().Format("2006-01-02") + ".log"
+
+	// lumberjack.Logger is already safe for concurrent use, so we don't need to lock it.
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:  logFile, // log filename
+		MaxSize:   maxSize, // megabytes
+		MaxAge:    maxAge,  // days
+		LocalTime: true,    // use the local machine's timezone
+	})
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		w,
+		zap.InfoLevel,
+	)
+	return zap.New(core)
+}
