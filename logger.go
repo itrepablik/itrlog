ddc0848e81968b77395a4b5f24ceabce19341d30
ITRepablik <itrepablik@gmail.com>
2020-02-28 14:11:19 +0800

    Add files via upload

--- logger.go
+++ logger.go
@@ -15,7 +15,7 @@ const LogTimeFormat string = "Jan-02-2006 03:04:05 PM"
 
 // InitLog initialize the zap and lumberjack logger library.
 // Example log file: logs/app_name_2020-02-28.log
-func InitLog(maxSizeInMB, maxAgeInDays int, logFolderName, logInitial string) (*zap.Logger, *zap.SugaredLogger) {
+func InitLog(maxSizeInMB, maxAgeInDays int, logFolderName, logInitial string) *zap.SugaredLogger {
 	logFile := logFolderName + "/" + logInitial + time.Now().Format("2006-01-02") + ".log"
 
 	// lumberjack.Logger is already safe for concurrent use, so we don't need to lock it.
@@ -33,5 +33,5 @@ func InitLog(maxSizeInMB, maxAgeInDays int, logFolderName, logInitial string) (*
 
 	logger := zap.New(core)
 	sugar := logger.Sugar()
-	return logger, sugar
+	return sugar
 }
