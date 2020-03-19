33c9655c6d36e48e5e7b7db5bcc63af563cc6e69
ITRepablik <itrepablik@gmail.com>
2020-03-19 20:52:23 +0800

    Update logger.go

--- logger.go
+++ logger.go
@@ -2,19 +2,30 @@
 package itrlog
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/itrepablik/lumberjack"
 
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+const (
+	_oddNumberErrMsg    = "Ignored key without a value."
+	_nonStringKeyErrMsg = "Ignored key-value pairs with non-string keys."
+)
+
+// Declared zap sugared logger
+var zapSugar *zap.SugaredLogger
 
 // ITRLogger contains the necessary information to initialize the required parameters for the itrlog package.
 type ITRLogger struct {
 	MaxSizeInMB, MaxAgeInDays int
 	LogFolderName, LogInitial string
 	Sugar                     *zap.SugaredLogger
+	Base                      *zap.Logger
 }
 
 // LogTimeFormat formats the event timestamp.
@@ -40,63 +51,70 @@ func InitLog(maxSizeInMB, maxAgeInDays int, logFolderName, logInitial string) *I
 
 	logger := zap.New(core)
 	sugar := logger.Sugar()
-	return &ITRLogger{MaxSizeInMB: maxSizeInMB, MaxAgeInDays: maxAgeInDays, LogFolderName: logFolderName, LogInitial: logInitial, Sugar: sugar}
+	return &ITRLogger{
+		MaxSizeInMB:   maxSizeInMB,
+		MaxAgeInDays:  maxAgeInDays,
+		LogFolderName: logFolderName,
+		LogInitial:    logInitial,
+		Sugar:         sugar,
+		Base:          logger,
+	}
 }
 
 // Debug uses fmt.Sprint to construct and log a message.
 func (s *ITRLogger) Debug(args ...interface{}) {
-	s.Sugar.Debug(args)
+	s.log(zap.DebugLevel, "", args, nil)
 }
 
 // Info uses fmt.Sprint to construct and log a message.
 func (s *ITRLogger) Info(args ...interface{}) {
-	s.Sugar.Info(args)
+	s.log(zap.InfoLevel, "", args, nil)
 }
 
 // Warn uses fmt.Sprint to construct and log a message.
 func (s *ITRLogger) Warn(args ...interface{}) {
-	s.Sugar.Warn(args)
+	s.log(zap.WarnLevel, "", args, nil)
 }
 
 // Error uses fmt.Sprint to construct and log a message.
 func (s *ITRLogger) Error(args ...interface{}) {
-	s.Sugar.Error(args)
+	s.log(zap.ErrorLevel, "", args, nil)
 }
 
 // DPanic uses fmt.Sprint to construct and log a message. In development, the
 // logger then panics. (See DPanicLevel for details.)
 func (s *ITRLogger) DPanic(args ...interface{}) {
-	s.Sugar.DPanic(args)
+	s.log(zap.DPanicLevel, "", args, nil)
 }
 
 // Panic uses fmt.Sprint to construct and log a message, then panics.
 func (s *ITRLogger) Panic(args ...interface{}) {
-	s.Sugar.Panic(args)
+	s.log(zap.PanicLevel, "", args, nil)
 }
 
 // Fatal uses fmt.Sprint to construct and log a message, then calls os.Exit.
 func (s *ITRLogger) Fatal(args ...interface{}) {
-	s.Sugar.Fatal(args)
+	s.log(zap.FatalLevel, "", args, nil)
 }
 
 // Debugf uses fmt.Sprintf to log a templated message.
 func (s *ITRLogger) Debugf(template string, args ...interface{}) {
-	s.Sugar.Debugf(template, args)
+	s.log(zap.DebugLevel, template, args, nil)
 }
 
 // Infof uses fmt.Sprintf to log a templated message.
 func (s *ITRLogger) Infof(template string, args ...interface{}) {
-	s.Sugar.Infof(template, args)
+	s.log(zap.InfoLevel, template, args, nil)
 }
 
 // Warnf uses fmt.Sprintf to log a templated message.
 func (s *ITRLogger) Warnf(template string, args ...interface{}) {
-	s.Sugar.Infof(template, args)
+	s.log(zap.WarnLevel, template, args, nil)
 }
 
 // Errorf uses fmt.Sprintf to log a templated message.
 func (s *ITRLogger) Errorf(template string, args ...interface{}) {
-	s.Sugar.Errorf(template, args)
+	s.log(zap.ErrorLevel, template, args, nil)
 }
 
 // DPanicf uses fmt.Sprintf to log a templated message. In development, the
@@ -107,12 +125,12 @@ func (s *ITRLogger) DPanicf(template string, args ...interface{}) {
 
 // Panicf uses fmt.Sprintf to log a templated message, then panics.
 func (s *ITRLogger) Panicf(template string, args ...interface{}) {
-	s.Sugar.Panicf(template, args)
+	s.log(zap.DPanicLevel, template, args, nil)
 }
 
 // Fatalf uses fmt.Sprintf to log a templated message, then calls os.Exit.
 func (s *ITRLogger) Fatalf(template string, args ...interface{}) {
-	s.Sugar.Fatalf(template, args)
+	s.log(zap.PanicLevel, template, args, nil)
 }
 
 // Debugw logs a message with some additional context. The variadic key-value
@@ -121,42 +139,130 @@ func (s *ITRLogger) Fatalf(template string, args ...interface{}) {
 // When debug-level logging is disabled, this is much faster than
 //  s.With(keysAndValues).Debug(msg)
 func (s *ITRLogger) Debugw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Debugw(msg, keysAndValues)
+	s.log(zap.DebugLevel, msg, nil, keysAndValues)
 }
 
 // Infow logs a message with some additional context. The variadic key-value
 // pairs are treated as they are in With.
 func (s *ITRLogger) Infow(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Infow(msg, keysAndValues)
+	s.log(zap.InfoLevel, msg, nil, keysAndValues)
 }
 
 // Warnw logs a message with some additional context. The variadic key-value
 // pairs are treated as they are in With.
 func (s *ITRLogger) Warnw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Warnw(msg, keysAndValues)
+	s.log(zap.WarnLevel, msg, nil, keysAndValues)
 }
 
 // Errorw logs a message with some additional context. The variadic key-value
 // pairs are treated as they are in With.
 func (s *ITRLogger) Errorw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Errorw(msg, keysAndValues)
+	s.log(zap.ErrorLevel, msg, nil, keysAndValues)
 }
 
 // DPanicw logs a message with some additional context. In development, the
 // logger then panics. (See DPanicLevel for details.) The variadic key-value
 // pairs are treated as they are in With.
 func (s *ITRLogger) DPanicw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.DPanicw(msg, keysAndValues)
+	s.log(zap.DPanicLevel, msg, nil, keysAndValues)
 }
 
 // Panicw logs a message with some additional context, then panics. The
 // variadic key-value pairs are treated as they are in With.
 func (s *ITRLogger) Panicw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Panicw(msg, keysAndValues)
+	s.log(zap.PanicLevel, msg, nil, keysAndValues)
 }
 
 // Fatalw logs a message with some additional context, then calls os.Exit. The
 // variadic key-value pairs are treated as they are in With.
 func (s *ITRLogger) Fatalw(msg string, keysAndValues ...interface{}) {
-	s.Sugar.Fatalw(msg, keysAndValues)
+	s.log(zap.FatalLevel, msg, nil, keysAndValues)
+}
+
+func (s *ITRLogger) log(lvl zapcore.Level, template string, fmtArgs []interface{}, context []interface{}) {
+	// If logging at this level is completely disabled, skip the overhead of
+	// string formatting.
+	if lvl < zap.DPanicLevel && !s.Base.Core().Enabled(lvl) {
+		return
+	}
+
+	// Format with Sprint, Sprintf, or neither.
+	msg := template
+	if msg == "" && len(fmtArgs) > 0 {
+		msg = fmt.Sprint(fmtArgs...)
+	} else if msg != "" && len(fmtArgs) > 0 {
+		msg = fmt.Sprintf(template, fmtArgs...)
+	}
+
+	if ce := s.Base.Check(lvl, msg); ce != nil {
+		ce.Write(s.sweetenFields(context)...)
+	}
+}
+
+func (s *ITRLogger) sweetenFields(args []interface{}) []zap.Field {
+	if len(args) == 0 {
+		return nil
+	}
+
+	// Allocate enough space for the worst case; if users pass only structured
+	// fields, we shouldn't penalize them with extra allocations.
+	fields := make([]zap.Field, 0, len(args))
+	var invalid invalidPairs
+
+	for i := 0; i < len(args); {
+		// This is a strongly-typed field. Consume it and move on.
+		if f, ok := args[i].(zap.Field); ok {
+			fields = append(fields, f)
+			i++
+			continue
+		}
+
+		// Make sure this element isn't a dangling key.
+		if i == len(args)-1 {
+			s.Base.DPanic(_oddNumberErrMsg, zap.Any("ignored", args[i]))
+			break
+		}
+
+		// Consume this value and the next, treating them as a key-value pair. If the
+		// key isn't a string, add this pair to the slice of invalid pairs.
+		key, val := args[i], args[i+1]
+		if keyStr, ok := key.(string); !ok {
+			// Subsequent errors are likely, so allocate once up front.
+			if cap(invalid) == 0 {
+				invalid = make(invalidPairs, 0, len(args)/2)
+			}
+			invalid = append(invalid, invalidPair{i, key, val})
+		} else {
+			fields = append(fields, zap.Any(keyStr, val))
+		}
+		i += 2
+	}
+
+	// If we encountered any invalid key-value pairs, log an error.
+	if len(invalid) > 0 {
+		s.Base.DPanic(_nonStringKeyErrMsg, zap.Array("invalid", invalid))
+	}
+	return fields
+}
+
+type invalidPair struct {
+	position   int
+	key, value interface{}
+}
+
+func (p invalidPair) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddInt64("position", int64(p.position))
+	zap.Any("key", p.key).AddTo(enc)
+	zap.Any("value", p.value).AddTo(enc)
+	return nil
+}
+
+type invalidPairs []invalidPair
+
+func (ps invalidPairs) MarshalLogArray(enc zapcore.ArrayEncoder) error {
+	var err error
+	for i := range ps {
+		err = multierr.Append(err, enc.AppendObject(ps[i]))
+	}
+	return err
 }
